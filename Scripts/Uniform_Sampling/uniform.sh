bc834f158c2a655770a6bdc616106c3a358ec3e6
Claire Jin <claireji@andrew.cmu.edu>
2023-01-10 18:44:46 -0800

    modify data and save paths

--- Scripts/Uniform_Sampling/uniform.sh
+++ Scripts/Uniform_Sampling/uniform.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
-DIR=../../Data/SumMe/videos/;
-OUT=../../Results/SumMe/Uniform_Sampling/;
+DIR=/data/jielin/msmo/video/;
+OUT=/home/jielin/claire/video-summ/keyframes/uniform/;
 HOMEDIR=$PWD;
 sampling_rate="1";
 
 for percent in "15"; do
 	echo $percent
-	for filename in $DIR*".mp4";do
-		echo $filename
-		cd $HOMEDIR
-		name=${filename##*/};
-		folder_name=${name%.mp4};
-		echo "printing previous shit"
-		echo $folder_name
-		mkdir $OUT$folder_name;
-		echo $OUT$folder_name
-		python uniform.py $filename $percent $OUT$folder_name"/";
-		cd ../Evaluation
-		pwd 
-		echo "printing the stuff"
-		echo $filename
-		echo $sampling_rate
-		echo $percent
-		echo $OUT$folder_name"/" $OUT$folder_name"/final_results_uniform_"$percent".txt"
-		python evaluate.py $filename $sampling_rate $percent $OUT$folder_name"/" $OUT$folder_name"/final_results_uniform_"$percent".txt" uniform;
+	for category in $DIR*/;do 
+		echo $category
+		for subcat in $category*/;do
+			echo $subcat
+			for video in $subcat*".mp4";do
+				# echo $video
+				cd $HOMEDIR
+				name=${video##*/};
+				folder_name=${name%.mp4};
+				# echo $name
+				# echo $folder_name
+				trunc=$(dirname "$subcat")
+				subfolder=$(basename "$trunc")/$(basename "$subcat")
+				# mkdir -p $OUT$subfolder/$folder_name
+				echo $OUT$subfolder/$folder_name
+				# python uniform.py $video $percent $OUT$subfolder/$folder_name/;
+				
+				# cd ../Evaluation
+				# pwd 
+				# echo "printing the stuff"
+				# echo $filename
+				# echo $sampling_rate
+				# echo $percent
+				# echo $OUT$folder_name"/" $OUT$folder_name"/final_results_uniform_"$percent".txt"
+				# python evaluate.py $filename $sampling_rate $percent $OUT$folder_name"/" $OUT$folder_name"/final_results_uniform_"$percent".txt" uniform;
+			done
+		done
 	done
 done
